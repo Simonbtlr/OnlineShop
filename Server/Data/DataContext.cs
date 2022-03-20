70442925182fea43cc58e29b914f3fdefc044389
simon <prog@dowell.ru>
2022-03-20 19:46:06 +0300

    Add the DataContext

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,8 @@
+namespace OnlineShop.Server.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    
+    public DbSet<Product> Products { get; set; }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,4 @@
+global using Microsoft.EntityFrameworkCore;
 global using OnlineShop.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
