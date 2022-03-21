63ff80dbc74f524d31344f15d3d44b5239e8cec1
simon <prog@dowell.ru>
2022-03-21 19:27:48 +0300

    Add the ProductService to the Client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,14 @@
+global using OnlineShop.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OnlineShop.Client;
+using OnlineShop.Client.Services.ProductService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
---
+++ Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,7 @@
+namespace OnlineShop.Client.Services.ProductService;
+
+public interface IProductService
+{
+    List<Product> Products { get; set; }
+    Task GetProducts();
+}
