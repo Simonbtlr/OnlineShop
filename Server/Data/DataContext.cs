0c07cb825ee65647d875faf4d6aa16e5db8b1af2
simon <prog@dowell.ru>
2022-03-21 23:21:03 +0300

    Add a Composite Primary Key & More Seeding

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -6,6 +6,20 @@ public class DataContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<ProductVariant>().HasKey(x => new { x.ProductId, x.ProductTypeId });
+
+        modelBuilder.Entity<ProductType>().HasData(
+            new ProductType { Id = 1, Name = "По умолчанию"},
+            new ProductType { Id = 2, Name = "В мягкой обложке"},
+            new ProductType { Id = 3, Name = "Электронная книга"},
+            new ProductType { Id = 4, Name = "Аудиокнига"},
+            new ProductType { Id = 5, Name = "Стрим"},
+            new ProductType { Id = 6, Name = "Blu-ray"},
+            new ProductType { Id = 7, Name = "VHS"},
+            new ProductType { Id = 8, Name = "PC"},
+            new ProductType { Id = 9, Name = "PlayStation"},
+            new ProductType { Id = 10, Name = "Xbox"});
+        
         modelBuilder.Entity<Category>().HasData(
             new Category
             {
@@ -41,7 +55,6 @@ public class DataContext : DbContext
                               "\n" +
                               "Книга включает в себя выпуски комиксов #1-15, а также дополнительные материалы!",
                 ImageUrl = "https://cdn1.ozone.ru/s3/multimedia-4/wc1200/6040304632.jpg",
-                Price = 1645,
                 CategoryId = 1
             },
             new Product
@@ -60,7 +73,6 @@ public class DataContext : DbContext
                               "мультсериал и не оставит равнодушным ни одного любителя отборного " +
                               "интеллектуально-трешёвого юмора и научной фантастики.",
                 ImageUrl = "https://cdn1.ozone.ru/s3/multimedia-t/wc1200/6100384409.jpg",
-                Price = 1830,
                 CategoryId = 1
             },
             new Product
@@ -72,7 +84,6 @@ public class DataContext : DbContext
                               "любимого героя сами и дарите книгу друзьям — несите слово Рика в массы, и да пребудет " +
                               "с вами наука! Вабба лабба даб даб!",
                 ImageUrl = "https://cdn1.ozone.ru/s3/multimedia-h/wc1200/6064729421.jpg",
-                Price = 975,
                 CategoryId = 1
             },
             new Product
@@ -83,7 +94,6 @@ public class DataContext : DbContext
                               "про рабочие будни.",
                 ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/7bbd225f-e6db-4326-b600-1ac294" +
                            "cf9d99/1920x",
-                Price = 299,
                 CategoryId = 2
             },
             new Product
@@ -94,7 +104,6 @@ public class DataContext : DbContext
                               "истории с метким юмором.",
                 ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/74925575-feba-41d1-880b-cb0eb2" +
                            "c8bf31/1920x",
-                Price = 299,
                 CategoryId = 2
             },
             new Product
@@ -105,7 +114,6 @@ public class DataContext : DbContext
                               "внутренней кухне стартапов.",
                 ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/009c26e0-7336-4a70-829b-682bbe" +
                            "119d4d/1920x",
-                Price = 299,
                 CategoryId = 2
             },
             new Product
@@ -116,7 +124,6 @@ public class DataContext : DbContext
                               "реальности. Это история невозможной борьбы с жестокой расой пришельцев, известной как " +
                               "Альянс. События происходят между Half-Life и Half-Life 2.",
                 ImageUrl = "https://upload.wikimedia.org/wikipedia/ru/b/bb/Half-Life_Alyx_Coverart.jpg",
-                Price = 1085,
                 CategoryId = 3
             },
             new Product
@@ -128,7 +135,6 @@ public class DataContext : DbContext
                               "реализован одним из самых замысловатых и разветвленных сюжетов из когда-либо " +
                               "созданных в игровой индустрии.",
                 ImageUrl = "https://upload.wikimedia.org/wikipedia/ru/e/ee/Detroit_Become_Human.jpg",
-                Price = 1628,
                 CategoryId = 3
             },
             new Product
@@ -138,7 +144,6 @@ public class DataContext : DbContext
                 Description = "Игра в жанре выживание, в которой вам предстоит исследовать огромный фэнтезийный мир, " +
                               "пропитанный скандинавской мифологией и культурой викингов.",
                 ImageUrl = "https://elamigosedition.com/uploads/posts/2021-02/1613136021_valheim-cover-download.webp",
-                Price = 435,
                 CategoryId = 3
             },
             new Product
@@ -148,7 +153,6 @@ public class DataContext : DbContext
                 Description = "Beat Saber — это VR-ритм-игра, в которой вы режете ритмы адреналиновой музыки, когда " +
                               "они летят к вам, окруженные футуристическим миром.",
                 ImageUrl = "https://i.redd.it/roocnhykls851.png",
-                Price = 515,
                 CategoryId = 3
             },
             new Product
@@ -160,8 +164,119 @@ public class DataContext : DbContext
                               "исследуйте незнакомую планету, возводите многоэтажные заводы и вступите в конвейерный " +
                               "рай!",
                 ImageUrl = "https://cdn.ananasposter.ru/image/cache/catalog/poster/games/80/26094-1000x830.jpg",
-                Price = 599,
                 CategoryId = 3
+            });
+
+        modelBuilder.Entity<ProductVariant>().HasData(
+            new ProductVariant
+            {
+                ProductId = 1,
+                ProductTypeId = 2,
+                Price = 1565.75m,
+                OriginalPrice = 1645
+            },
+            new ProductVariant
+            {
+                ProductId = 1,
+                ProductTypeId = 3,
+                Price = 1530.85m
+            },
+            new ProductVariant
+            {
+                ProductId = 1,
+                ProductTypeId = 4,
+                Price = 1480.5m,
+                OriginalPrice = 1645
+            },
+            new ProductVariant
+            {
+                ProductId = 2,
+                ProductTypeId = 2,
+                Price = 1740.5m,
+                OriginalPrice = 1830
+            }, 
+            new ProductVariant 
+            {
+                ProductId = 3, 
+                ProductTypeId = 2, 
+                Price = 925.25m
+            }, 
+            new ProductVariant 
+            {
+                ProductId = 4,
+                ProductTypeId = 5,
+                Price = 225.25m
+            },
+            new ProductVariant
+            {
+                ProductId = 4,
+                ProductTypeId = 6,
+                Price = 290
+            },
+            new ProductVariant
+            {
+                ProductId = 4,
+                ProductTypeId = 7,
+                Price = 149.5m
+            },
+            new ProductVariant
+            {
+                ProductId = 5,
+                ProductTypeId = 5,
+                Price = 225.25m,
+            },
+            new ProductVariant
+            {
+                ProductId = 6,
+                ProductTypeId = 5,
+                Price = 225.5m
+            },
+            new ProductVariant
+            {
+                ProductId = 7,
+                ProductTypeId = 8,
+                Price = 920.25m,
+                OriginalPrice = 1085
+            },
+            new ProductVariant
+            {
+                ProductId = 7,
+                ProductTypeId = 9,
+                Price = 1030.75m
+            },
+            new ProductVariant
+            {
+                ProductId = 7,
+                ProductTypeId = 10,
+                Price = 1009.05m,
+                OriginalPrice = 1085
+            },
+            new ProductVariant
+            {
+                ProductId = 8,
+                ProductTypeId = 8,
+                Price = 1383.8m,
+                OriginalPrice = 1628,
+            },
+            new ProductVariant
+            {
+                ProductId = 9,
+                ProductTypeId = 8,
+                Price = 369.75m
+            },
+            new ProductVariant
+            {
+                ProductId = 10,
+                ProductTypeId = 1,
+                Price = 360.5m,
+                OriginalPrice = 515
+            },
+            new ProductVariant
+            {
+                ProductId = 11,
+                ProductTypeId = 1,
+                Price = 419.3m,
+                OriginalPrice = 599
             });
         
         base.OnModelCreating(modelBuilder);
@@ -169,4 +284,6 @@ public class DataContext : DbContext
 
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<ProductType> ProductTypes { get; set; }
+    public DbSet<ProductVariant> ProductVariants { get; set; }
 }
