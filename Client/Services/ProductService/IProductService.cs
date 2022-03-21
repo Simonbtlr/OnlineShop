6cfa5994a9b4dd7a11de4374df1feb0c63adae7b
simon <prog@dowell.ru>
2022-03-21 22:24:55 +0300

    Get Products by Category on the Client

--- Client/Services/ProductService/IProductService.cs
+++ Client/Services/ProductService/IProductService.cs
@@ -2,7 +2,8 @@ namespace OnlineShop.Client.Services.ProductService;
 
 public interface IProductService
 {
+    event Action ProductsChanged;
     List<Product> Products { get; set; }
-    Task GetProductsAsync();
+    Task GetProductsAsync(string? categoryUrl = null);
     Task<ServiceResponse<Product>> GetProductAsync(int productId);
 }
--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -3,6 +3,8 @@ namespace OnlineShop.Client.Services.ProductService;
 public class ProductService : IProductService
 {
     private readonly HttpClient _httpClient;
+
+    public event Action ProductsChanged;
     
     public List<Product> Products { get; set; } = new();
 
@@ -11,12 +13,17 @@ public class ProductService : IProductService
         _httpClient = httpClient;
     }
     
-    public async Task GetProductsAsync()
+    public async Task GetProductsAsync(string? categoryUrl = null)
     {
-        var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product");
+        var result = categoryUrl == null 
+            ? await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product")
+            : await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>
+                ($"api/Product/category/{categoryUrl}");
 
         if (result?.Data != null)
             Products = result.Data;
+        
+        ProductsChanged.Invoke();
     }
 
     public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
