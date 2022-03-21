1750ca5495af144a83dcd8c3136f25593897c249
simon <prog@dowell.ru>
2022-03-22 00:25:45 +0300

    Add Search Methods to the Client

--- Client/Services/ProductService/IProductService.cs
+++ Client/Services/ProductService/IProductService.cs
@@ -4,6 +4,9 @@ public interface IProductService
 {
     event Action ProductsChanged;
     List<Product> Products { get; set; }
+    string Message { get; set; }
     Task GetProductsAsync(string? categoryUrl = null);
     Task<ServiceResponse<Product>> GetProductAsync(int productId);
+    Task SearchProductsAsync(string searchText);
+    Task<List<string>> GetProductSearchSuggestionsAsync(string searchText);
 }
--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -2,22 +2,24 @@ namespace OnlineShop.Client.Services.ProductService;
 
 public class ProductService : IProductService
 {
-    private readonly HttpClient _httpClient;
+    private readonly HttpClient _http;
 
     public event Action ProductsChanged;
     
     public List<Product> Products { get; set; } = new();
 
-    public ProductService(HttpClient httpClient)
+    public string Message { get; set; } = "Загрузка товаров...";
+
+    public ProductService(HttpClient http)
     {
-        _httpClient = httpClient;
+        _http = http;
     }
     
     public async Task GetProductsAsync(string? categoryUrl = null)
     {
         var result = categoryUrl == null 
-            ? await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product")
-            : await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>
+            ? await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product")
+            : await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>
                 ($"api/Product/category/{categoryUrl}");
 
         if (result?.Data != null)
@@ -28,7 +30,29 @@ public class ProductService : IProductService
 
     public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
     {
-        var result = await _httpClient.GetFromJsonAsync<ServiceResponse<Product>>($"api/Product/{productId}");
+        var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/Product/{productId}");
         return result;
+    }
+
+    public async Task SearchProductsAsync(string searchText)
+    {
+        var result =
+            await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/Product/search/{searchText}");
+
+        if (result?.Data is not null)
+            Products = result.Data;
+
+        if (Products.Count == 0)
+            Message = "Товары не найдены.";
+        
+        ProductsChanged.Invoke();
+    }
+
+    public async Task<List<string>> GetProductSearchSuggestionsAsync(string searchText)
+    {
+        var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>
+                ($"api/Product/searchsuggestions/{searchText}");
+
+        return result.Data;
     }
 }
