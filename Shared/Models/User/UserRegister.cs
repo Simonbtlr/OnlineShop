8b7a9f193b33744e4d3f2501133006d7efbe9e22
simon <prog@dowell.ru>
2022-03-27 18:52:37 +0300

    Add Data Annotations for Validations

--- Shared/Models/User/UserRegister.cs
+++ Shared/Models/User/UserRegister.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OnlineShop.Shared.Models.User;
 
 public class UserRegister
 {
+    [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
+    [Required, StringLength(100, MinimumLength = 6)]
     public string Password { get; set; } = string.Empty;
+    [Compare("Password", ErrorMessage = "Пароли не совпадают")]
     public string ConfirmPassword { get; set; } = string.Empty;
 }
